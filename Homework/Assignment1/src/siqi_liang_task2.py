0b8e741c435eaecfc16ab86ed547eb471f16d9a2
AgentDS <zszx0259@me.com>
2019-09-21 00:00:52 -0700

    add output for HW1-task2

--- Homework/Assignment1/src/siqi_liang_task2.py
+++ Homework/Assignment1/src/siqi_liang_task2.py
@@ -7,6 +7,8 @@
 # @Software: PyCharm
 
 import sys
+import json
+from pyspark import SparkContext
 
 if __name__ == '__main__':
     argv = sys.argv
@@ -16,3 +18,12 @@ if __name__ == '__main__':
     print("Input: ", user_file)
     print("Output: ", output_file)
     print("n_partition: ", n_partition)
+
+    task2_json = {
+        "default": {"n_partition": default_n_partition, "n_items": default_n_items, "exe_time": defualt_exe_time},
+        "customized": {"n_partition": customized_n_partition, "n_items": customized_n_items,
+                       "exe_time": customized_exe_time},
+        "explanation": explanation
+    }
+    with open(output_file, "w") as write_file:
+        json.dump(task2_json, write_file)
