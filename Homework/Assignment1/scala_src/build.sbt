55bada90a0fcc1ebb91f927e1dbf48b1e0ca1b06
AgentDS <zszx0259@me.com>
2019-09-25 17:46:21 -0700

    ..

--- Homework/Assignment1/scala_src/build.sbt
+++ Homework/Assignment1/scala_src/build.sbt
@@ -5,3 +5,5 @@ version := "0.1"
 scalaVersion := "2.11.8"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.3"
+
+libraryDependencies += "com.typesafe.play" %% "play-json" % "2.7.3"
--- Homework/Assignment1/scala_src/src/main/scala/siqi_liang_task1.scala
+++ Homework/Assignment1/scala_src/src/main/scala/siqi_liang_task1.scala
@@ -1,8 +1,48 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
+import play.api.libs.json._
 
-object hw1Task1 {
+case class UserInfo(
+		user_id: String,
+	    name: String,
+	    review_count: Int,
+	    yelping_since: String,  // like '2013-02-21 22:29:06'
+	    useful: Int,
+	    funny: Int,
+	    cool: Int,
+	    elite: String,  // like '2015,2016,2017'
+	    friends: String,  // like 'xx, xxx, xxx'
+	    fans: Int,
+	    average_stars: Float,
+	    compliment_hot: Int,
+	    compliment_more: Int,
+	    compliment_profile: Int,
+	    compliment_cute: Int,
+	    compliment_list: Int,
+	    compliment_note: Int,
+	    compliment_plain: Int,
+	    compliment_cool: Int,
+	    compliment_funny: Int,
+	    compliment_writer: Int,
+	    compliment_photos: Int
+)
+
+
+object siqi_liang_task1 {
+
+	// implicit val formats = DefaultFormats
+
+    // def json_parse(jsonObj: String) : UserInfo = {
+    // 	val jValue = parse(jsonObj)
+    // 	val user = jValue.extract[UserInfo]
+    // 	return user
+    // }
+
+
 	def main(args: Array[String]) {
+		// val userFile = args(0)
+		// val outFile = args(1)
+		
 		val path = "/Users/liangsiqi/Documents/Dataset/yelp_dataset/"
         val userFile = "user.json"
         val reviewFile = "review.json"
@@ -10,7 +50,8 @@ object hw1Task1 {
 
         val sc = new SparkContext()
 		val userRDD = sc.textFile(path + userFile)
-		val userRDDjson = userRDD.map(json.loads)
+		val userRDDjson = userRDD.map(x => Json.parse(x))
+		println(userRDD.take(1).JsValue)
 
 	}
 }
