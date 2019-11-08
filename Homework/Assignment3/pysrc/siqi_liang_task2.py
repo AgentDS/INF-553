805f41352ce7839881d8b35ee46680b4383379cc
AgentDS <zszx0259@me.com>
2019-11-07 16:52:39 -0800

    hw3 task2 model-based finished, yelp_val.csv local test RMSE=1.06646, CPU time=17.60s

--- Homework/Assignment3/pysrc/siqi_liang_task2.py
+++ Homework/Assignment3/pysrc/siqi_liang_task2.py
@@ -7,6 +7,116 @@
 # @Software: PyCharm
 
 import sys
+from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
+from pyspark import SparkContext, SparkConf
+import csv
+from time import time
+
+
+def emit_idx_rating(lines, uid2idx_bc, bid2idx_bc):
+    for line in lines:
+        yield Rating(uid2idx_bc.value[line[0]], bid2idx_bc.value[line[1]], line[2])
+
+
+def emit_existing_pair(lines, uid2idx_bc, bid2idx_bc):
+    for line in lines:
+        if line[0] in uid2idx_bc.value.keys():
+            if line[1] in bid2idx_bc.value.keys():
+                yield (uid2idx_bc.value[line[0]], bid2idx_bc.value[line[1]])
+
+
+def emit_missing_pair(lines, uid2idx_bc, bid2idx_bc):
+    for line in lines:
+        if line[0] not in uid2idx_bc.value.keys() or line[1] not in bid2idx_bc.value.keys():
+            yield (line[0], line[1])
+
+
+def emit_id_rating_pairs(lines, uidx2id_bc, bidx2id_bc):
+    for line in lines:
+        yield [uidx2id_bc.value[line[0]], bidx2id_bc.value[line[1]], line[2]]
+
+
+def emit_avg_pairs(lines, avg_bc):
+    for line in lines:
+        yield [line[0], line[1], avg_bc.value]
+
+
+def model_based_CF(train_file_path, test_file_path, output_file_path):
+    raw_train = []
+    with open(train_file_path, 'r') as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                raw_train.append([row[0], row[1], float(row[2])])
+
+    raw_test = []
+    with open(test_file_path, 'r') as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                raw_test.append([row[0], row[1]])
+
+    # [user_id, business_id, stars]
+    conf = SparkConf().set('spark.driver.host', '127.0.0.1')
+    sc = SparkContext(master='local', appName='myAppName', conf=conf)
+    train_data = sc.parallelize(raw_train, 5)
+    test_data = sc.parallelize(raw_test, 5)
+
+    avg_train = train_data.map(lambda x: x[2]).mean()
+    avg_train_bc = sc.broadcast(avg_train)
+
+    user_ids = train_data.map(lambda x: x[0]).distinct().collect()
+    user_cnt = len(user_ids)
+    business_ids = train_data.map(lambda x: x[1]).distinct().collect()
+    business_cnt = len(business_ids)
+
+    uid2idx = dict()
+    bid2idx = dict()
+    uidx2id = dict()
+    bidx2id = dict()
+    for idx, uid in enumerate(user_ids, 0):
+        uid2idx[uid] = idx
+        uidx2id[idx] = uid
+
+    for idx, bid in enumerate(business_ids, 0):
+        bid2idx[bid] = idx
+        bidx2id[idx] = bid
+
+    uidx2id_bc = sc.broadcast(uidx2id)
+    bidx2id_bc = sc.broadcast(bidx2id)
+    uid2idx_bc = sc.broadcast(uid2idx)
+    bid2idx_bc = sc.broadcast(bid2idx)
+
+    train_ratings = train_data.mapPartitions(lambda lines: emit_idx_rating(lines, uid2idx_bc, bid2idx_bc))
+    model = ALS.train(train_ratings, 3, 15, 0.2)  # local optimal parameters
+
+    # Evaluate the model on validation data
+    exisitng_val_pairs = test_data.mapPartitions(lambda lines: emit_existing_pair(lines, uid2idx_bc, bid2idx_bc))
+    missing_val_pairs = test_data.mapPartitions(lambda lines: emit_missing_pair(lines, uid2idx_bc, bid2idx_bc))
+
+    # [uid,bid,rating]
+    existing_val_predictions = model.predictAll(exisitng_val_pairs).mapPartitions(
+        lambda lines: emit_id_rating_pairs(lines, uidx2id_bc, bidx2id_bc)).collect()
+    missing_val_predictions = missing_val_pairs.mapPartitions(lambda lines: emit_avg_pairs(lines, avg_train_bc)).collect()
+    sc.stop()
+
+    with open(output_file_path, 'w') as out_f:
+        print("user_id, business_id, prediction", file=out_f)
+        for line in existing_val_predictions:
+            print(line[0] + ',' + line[1] + ',' + "%.10f" % line[2], file=out_f)
+        for line in missing_val_predictions:
+            print(line[0] + ',' + line[1] + ',' + "%.10f" % line[2], file=out_f)
+
+
+def user_based_CF(train_file_path, test_file_path, output_file_path):
+    pass
+
 
 if __name__ == "__main__":
     argv = sys.argv
@@ -17,7 +127,8 @@ if __name__ == "__main__":
 
     if case_id == 1:
         # Model-based CF
-        pass
+        # local test CPU time: 17.60s
+        model_based_CF(train_file_path, test_file_path, output_file_path)
     elif case_id == 2:
         # User-based CF
         pass
--- Homework/Assignment3/pysrc/task2_experiment.py
+++ Homework/Assignment3/pysrc/task2_experiment.py
@@ -6,7 +6,7 @@
 # @File    : task2_experiment.py
 # @Software: PyCharm
 from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 import csv
 from time import time
 import itertools
@@ -21,21 +21,30 @@ def emit_idx_rating(lines, uid2idx_bc, bid2idx_bc):
         yield Rating(uid2idx_bc.value[line[0]], bid2idx_bc.value[line[1]], line[2])
 
 
-def emit_idx_rating_test(lines, uid2idx_bc, bid2idx_bc):
+def emit_existing_pair(lines, uid2idx_bc, bid2idx_bc):
     for line in lines:
         if line[0] in uid2idx_bc.value.keys():
             if line[1] in bid2idx_bc.value.keys():
-                yield Rating(uid2idx_bc.value[line[0]], bid2idx_bc.value[line[1]], line[2])
-            else:
-                yield Rating(uid2idx_bc.value[line[0]], -1, line[2])
-        else:
-            if line[1] in bid2idx_bc.value.keys():
-                yield Rating(-1, bid2idx_bc.value[line[1]], line[2])
-            else:
-                yield Rating(-1, -1, line[2])
+                yield (uid2idx_bc.value[line[0]], bid2idx_bc.value[line[1]])
 
 
-def run_all(numPartitions, rank, numIter, lmbda):
+def emit_missing_pair(lines, uid2idx_bc, bid2idx_bc):
+    for line in lines:
+        if line[0] not in uid2idx_bc.value.keys() or line[1] not in bid2idx_bc.value.keys():
+            yield (line[0], line[1])
+
+
+def emit_id_rating_pairs(lines, uidx2id_bc, bidx2id_bc):
+    for line in lines:
+        yield [uidx2id_bc.value[line[0]], bidx2id_bc.value[line[1]], line[2]]
+
+
+def emit_avg_pairs(lines, avg_bc):
+    for line in lines:
+        yield [line[0], line[1], avg_bc.value]
+
+
+def run_all(numPartitions, rank, numIter, lmbda, output_file):
     start = time()
     train_file = "/Users/liangsiqi/Documents/Dataset/yelp_rec_data/yelp_train.csv"
     val_file = "/Users/liangsiqi/Documents/Dataset/yelp_rec_data/yelp_val.csv"
@@ -58,12 +67,16 @@ def run_all(numPartitions, rank, numIter, lmbda):
             if line_count == 0:
                 line_count += 1
             else:
-                raw_val.append([row[0], row[1], float(row[2])])
+                raw_val.append([row[0], row[1]])
 
     # [user_id, business_id, stars]
-    sc = SparkContext.getOrCreate()
+    conf = SparkConf().set('spark.driver.host', '127.0.0.1')
+    sc = SparkContext(master='local', appName='myAppName', conf=conf)
     train_data = sc.parallelize(raw_train, numPartitions)
     val_data = sc.parallelize(raw_val, numPartitions)
+
+    avg_train = train_data.map(lambda x: x[2]).mean()
+    avg_train_bc = sc.broadcast(avg_train)
 
     user_ids = train_data.map(lambda x: x[0]).distinct().collect()
     user_cnt = len(user_ids)
@@ -72,53 +85,96 @@ def run_all(numPartitions, rank, numIter, lmbda):
 
     uid2idx = dict()
     bid2idx = dict()
-    idx2bid = dict()
+    uidx2id = dict()
+    bidx2id = dict()
     for idx, uid in enumerate(user_ids, 0):
         uid2idx[uid] = idx
+        uidx2id[idx] = uid
 
     for idx, bid in enumerate(business_ids, 0):
         bid2idx[bid] = idx
-        idx2bid[idx] = bid
+        bidx2id[idx] = bid
 
-    idx2bid_bc = sc.broadcast(idx2bid)
+    uidx2id_bc = sc.broadcast(uidx2id)
+    bidx2id_bc = sc.broadcast(bidx2id)
     uid2idx_bc = sc.broadcast(uid2idx)
     bid2idx_bc = sc.broadcast(bid2idx)
 
     train_ratings = train_data.mapPartitions(lambda lines: emit_idx_rating(lines, uid2idx_bc, bid2idx_bc))
-    val_ratings = val_data.mapPartitions(lambda lines: emit_idx_rating_test(lines, uid2idx_bc, bid2idx_bc))
-
     model = ALS.train(train_ratings, rank, numIter, lmbda)
 
     # Evaluate the model on training data
     train_pairs = train_ratings.map(lambda p: (p[0], p[1]))
     train_predictions = model.predictAll(train_pairs).map(lambda r: ((r[0], r[1]), r[2]))
     train_ratesAndPreds = train_ratings.map(lambda r: ((r[0], r[1]), r[2])).join(train_predictions)
-    train_RMSE = train_ratesAndPreds.map(lambda r: (r[1][0] - r[1][1]) ** 2).mean() ** 0.5
+    # train_RMSE = train_ratesAndPreds.map(lambda r: (r[1][0] - r[1][1]) ** 2).mean() ** 0.5
 
     # Evaluate the model on validation data
-    val_pairs = val_ratings.map(lambda p: (p[0], p[1]))
-    val_predictions = model.predictAll(val_pairs).map(lambda r: ((r[0], r[1]), r[2]))
-    val_ratesAndPreds = val_ratings.map(lambda r: ((r[0], r[1]), r[2])).join(val_predictions)
-    val_RMSE = val_ratesAndPreds.map(lambda r: (r[1][0] - r[1][1]) ** 2).mean() ** 0.5
+    exisitng_val_pairs = val_data.mapPartitions(lambda lines: emit_existing_pair(lines, uid2idx_bc, bid2idx_bc))
+    missing_val_pairs = val_data.mapPartitions(lambda lines: emit_missing_pair(lines, uid2idx_bc, bid2idx_bc))
 
+    # [uid,bid,rating]
+    existing_val_predictions = model.predictAll(exisitng_val_pairs).mapPartitions(
+        lambda lines: emit_id_rating_pairs(lines, uidx2id_bc, bidx2id_bc)).collect()
+    missing_val_predictions = missing_val_pairs.mapPartitions(lambda lines: emit_avg_pairs(lines, avg_train_bc)).collect()
+    sc.stop()
+
+    with open(output_file, 'w') as out_f:
+        print("user_id, business_id, prediction", file=out_f)
+        for line in existing_val_predictions:
+            print(line[0] + ',' + line[1] + ',' + "%.10f" % line[2], file=out_f)
+        for line in missing_val_predictions:
+            print(line[0] + ',' + line[1] + ',' + "%.10f" % line[2], file=out_f)
     end = time()
-    return train_RMSE, val_RMSE, int(end - start)
+    return int(end - start)
 
 
 if __name__ == "__main__":
-    ranks = [2, 3, 4, 5, 6, 7]
-    lambdas = [0.01, 0.02, 0.04, 0.08, 0.1, 0.2, 0.4, 0.8]
+    ranks = [2, 3, 4, 5]
+    lambdas = [0.08, 0.1, 0.2]
     numIters = [10, 15, 20]
-    partition_num = [2, 3, 4, 5, 7, 9]
+    partition_num = [3, 4, 5]
+    output_file = "./task2_local_res.csv"
 
     # TODO: rank 3 lambda 0.1, val_RMSE 1.07 ?????
-    with open("./task2_experiment.txt", 'w') as log_file:
-        print("rank         lambda         numIter         numPartitions         train_RMSE         val_RMSE         duration",
-              file=log_file)
-        print("rank         lambda         numIter         numPartitions         train_RMSE         val_RMSE         duration")
-        for rank, lmbda, numIter, numPartitions in itertools.product(ranks, lambdas, numIters, partition_num):
-            train_RMSE, val_RMSE, duration = run_all(numPartitions, rank, numIter, lmbda)
-            print("%2d           %4.3f            %2d                %2d                 %6.5f           %6.5f           %3d" % (
-                rank, lmbda, numIter, numPartitions, train_RMSE, val_RMSE, duration))
-            print("%2d           %4.3f            %2d                %2d                 %6.5f           %6.5f           %3d" % (
-                rank, lmbda, numIter, numPartitions, train_RMSE, val_RMSE, duration), file=log_file)
+    duration = run_all(numPartitions=5, rank=3, numIter=15, lmbda=0.2, output_file=output_file)
+    conf = SparkConf().set('spark.driver.host', '127.0.0.1')
+    sc = SparkContext(master='local', appName='myAppName', conf=conf)
+
+    raws = []
+    with open(output_file, 'r') as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                raws.append([row[0] + ',' + row[1], float(row[2])])
+    predict_val_pair = sc.parallelize(raws)
+
+    raws = []
+    val_file = "/Users/liangsiqi/Documents/Dataset/yelp_rec_data/yelp_val.csv"
+    with open(val_file, 'r') as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                raws.append([row[0] + ',' + row[1], float(row[2])])
+    ground_val_pair = sc.parallelize(raws)
+
+    val_RMSE = (ground_val_pair.join(predict_val_pair).map(lambda x: (x[1][0] - x[1][1]) ** 2).mean()) ** 0.5
+    print("val_RMSE = %.5f, duration = %ds" % (val_RMSE, duration))
+    sc.stop()
+
+# with open("./task2_experiment.txt", 'w') as log_file:
+# print("rank         lambda         numIter         numPartitions         train_RMSE         val_RMSE         duration", file=log_file)
+# print("rank         lambda         numIter         numPartitions         train_RMSE         val_RMSE         duration")
+# for rank, lmbda, numIter, numPartitions in itertools.product(ranks, lambdas, numIters, partition_num):
+#     duration = run_all(numPartitions, rank, numIter, lmbda, output_file)
+
+# print("%2d           %4.3f            %2d                %2d                 %6.5f           %6.5f           %3d" % (
+#     rank, lmbda, numIter, numPartitions, train_RMSE, val_RMSE, duration))
+# print("%2d           %4.3f            %2d                %2d                 %6.5f           %6.5f           %3d" % (
+#     rank, lmbda, numIter, numPartitions, train_RMSE, val_RMSE, duration), file=log_file)
